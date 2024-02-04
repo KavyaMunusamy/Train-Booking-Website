d8d93216db8b93ad0f6e5a6e0b3cd89cea6fc560
KavyaMunusamy <158807400+KavyaMunusamy@users.noreply.github.com>
2024-02-04 14:24:07 +0530

    Add files via upload

---
+++ train.js
@@ -0,0 +1,12 @@
+function changetext(){
+var textElement=document.getElementById("text");
+textElement.innerHTML="Thankyou for visitting our website";
+}
+
+
+setTimeout(myfunction,3000);
+
+function myfunction()
+{
+document.getElementById("time").innerHtML="Have a nice day!";
+}
